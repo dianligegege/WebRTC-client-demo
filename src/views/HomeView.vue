9d18bfcc33c857153110192b21359572538584d3
dianligegege <1220671124@qq.com>
2023-12-12 23:34:25 +0800

    fix: 伟大的开始

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Player />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Player from "@/components/VideoPlayer.vue";
+
+export default defineComponent({
+  name: "HomeView",
+  components: {
+    Player,
+  },
+});
+</script>
