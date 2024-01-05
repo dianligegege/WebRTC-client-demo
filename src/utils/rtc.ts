815f7468eded9bc63c8825e61a318c4a25e22803
zhangli <zhangli02@hongsong.club>
2024-01-05 19:28:57 +0800

    fix: rtc 打通

--- src/utils/rtc.ts
+++ src/utils/rtc.ts
@@ -2,9 +2,9 @@ type ClientType = 'local' | 'remote';
 
 interface IProp {
   clientType?: ClientType,
-  icecandidate?: (e: RTCIceCandidate) => void,
-  iceconnectionstatechange?: (e: Event) => void,
-  track?: (e: any) => void;
+  onIcecandidate?: (e: any) => void,
+  onIceconnectionstatechange?: (e: Event) => void,
+  onTrack?: (e: any) => void;
 }
 
 export default class WebRTCClient {
@@ -14,22 +14,22 @@ export default class WebRTCClient {
 
   constructor({
     clientType = 'local',
-    icecandidate,
-    iceconnectionstatechange,
-    track,
+    onIcecandidate,
+    onIceconnectionstatechange,
+    onTrack,
   }: IProp) {
     this.isLocal = clientType === 'local';
     this.pc = new RTCPeerConnection();
-    if (icecandidate) this.onIcecandidate(icecandidate);
-    if (iceconnectionstatechange) this.onIceconnectionstatechange(iceconnectionstatechange);
-    if (track) this.onTrack(track);
+    if (onIcecandidate) this.onIcecandidate(onIcecandidate);
+    if (onIceconnectionstatechange) this.onIceconnectionstatechange(onIceconnectionstatechange);
+    if (onTrack) this.onTrack(onTrack);
   }
 
   // icecandidate
   // 包含通信协议(TCP/UDP)和通信IP， STUN和TURN协议中描述网络信息的格式规范 ，解决双方网络链接问题
-  onIcecandidate = (cb: (e: RTCIceCandidate) => void) => {
+  onIcecandidate = (cb: (e: RTCPeerConnectionIceEvent) => void) => {
     this.pc?.addEventListener('icecandidate', async (e) => {
-      if (e?.candidate && cb) cb(e.candidate);
+      if (cb) cb(e);
     });
   }
 
@@ -53,22 +53,18 @@ export default class WebRTCClient {
   // 浏览器能力，包括不限于音视频编码格式，带宽，流控策略等
   createOffer = async () => {
     const offer = await this.pc?.createOffer();
-    this.pc?.setLocalDescription(offer);
+    await this.pc?.setLocalDescription(offer);
     return offer;
   }
 
   createAnswer = async () => {
     const answer = await this.pc?.createAnswer();
-    this.pc?.setLocalDescription(answer);
+    await this.pc?.setLocalDescription(answer);
     return answer;
   }
 
-  saveOffer = async (offer: any) => {
-    this.pc?.setRemoteDescription(offer);
-  }
-
-  saveAnswer = async (answer: any) => {
-    this.pc?.setRemoteDescription(answer);
+  saveRemoteDescription = async (des: any) => {
+    await this.pc?.setRemoteDescription(des);
   }
 
   // track
