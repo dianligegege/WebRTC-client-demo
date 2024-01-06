f084d8fa0c36b149f6099977b80de405e7f4984b
dianligegege <1220671124@qq.com>
2024-01-06 18:12:47 +0800

    fix: 添加share 和 audio

---
+++ src/views/SharePc.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="video-wrap">
+    <div class="btn-wrap">
+      <button @click="start">开始</button>
+      <button @click="close">关闭</button>
+    </div>
+    <div class="video local-video">
+      <video :id="lovalVideoId" autoplay muted></video>
+    </div>
+    <div class="video remote-video">
+      <video :id="remoteVideoId" autoplay muted></video>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+// import CustomVideoPlayer from '@/utils/video';
+import { nextTick, onMounted, reactive, ref } from 'vue';
+import WebRTCClient from '@/utils/rtc';
+import createPlayer from '@/utils/createPlayer';
+
+const lovalVideoId = 'localVideoId';
+const remoteVideoId = 'remoteVideoId';
+let remoteVideo: HTMLVideoElement;
+let localVideo: HTMLVideoElement;
+
+const localOptions = {
+  autoplay: true,
+  controls: false,
+  muted: true,
+};
+
+const localPlayer = ref();
+const remotePlayer = ref();
+
+const pcLocal: any = ref();
+const pcRemote: any = ref();
+let pc1: any;
+let pc2: any;
+
+let localStream: any;
+
+function handleRtc() {
+  pcLocal.value = new WebRTCClient({});
+  pcRemote.value = new WebRTCClient({
+    onTrack(e) {
+      if (e.streams.length > 0 && remoteVideo) {
+        [remoteVideo.srcObject] = e.streams;
+      }
+    },
+  });
+
+  pc1 = pcLocal.value?.pc;
+  pc2 = pcRemote.value?.pc;
+
+  pc1.addEventListener('icecandidate', async (e) => {
+    console.log('zl-icecandidate');
+    if (e.candidate) {
+      await pcRemote.value?.addIceCandidate(e.candidate);
+    }
+  });
+
+  pc2.addEventListener('icecandidate', async (e) => {
+    if (e.candidate) {
+      await pcLocal.value.addIceCandidate(e.candidate);
+    }
+  });
+}
+
+async function createOffer() {
+  const offer = await pcLocal.value?.createOffer();
+  await pcRemote.value?.saveRemoteDescription(offer);
+  const answer = await pcRemote.value?.createAnswer();
+  await pcLocal.value?.saveRemoteDescription(answer);
+}
+
+async function handlePlayer() {
+  localPlayer.value = createPlayer({
+    videoElementId: lovalVideoId,
+    customOptions: localOptions,
+  });
+  remotePlayer.value = createPlayer({
+    videoElementId: remoteVideoId,
+    customOptions: localOptions,
+  });
+  remoteVideo = remotePlayer.value.el().querySelector('video') as HTMLVideoElement;
+  localVideo = localPlayer.value.el().querySelector('video') as HTMLVideoElement;
+}
+
+async function handleStream() {
+  localStream = await navigator.mediaDevices.getDisplayMedia({
+    video: true,
+    audio: true,
+  });
+
+  if (localVideo) {
+    localVideo.srcObject = localStream;
+  }
+  localStream.getTracks().forEach((track) => pc1.addTrack(track, localStream));
+}
+
+async function close() {
+  if (localStream) {
+    localStream.getTracks().forEach((track: any) => track.stop());
+  }
+  if (pc1) {
+    pc1.close();
+    pc1 = null;
+  }
+}
+
+async function start() {
+  close();
+  await handleRtc();
+  nextTick(async () => {
+    await handleStream();
+    await createOffer();
+  });
+}
+
+onMounted(async () => {
+  nextTick(async () => {
+    await handlePlayer();
+  });
+});
+</script>
+
+<style lang="scss">
+.video-wrap {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  .video {
+    margin: 16px;
+    width: 500px;
+    height: 282px;
+  }
+
+  .btn-wrap {
+    margin-bottom: 30px;
+
+    > button {
+      margin: 0 10px;
+    }
+  }
+}
+</style>
