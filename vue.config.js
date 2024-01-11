f73f1d241c0293127888f2cca69bb1f1986d2b90
zhangli <zhangli02@hongsong.club>
2024-01-11 20:47:42 +0800

    fix: 支持https

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service');
+const fs = require('fs');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    https: {
+      key: fs.readFileSync('./src/assets/localhost+2-key.pem'),
+      cert: fs.readFileSync('./src/assets/localhost+2.pem'),
+    },
+  },
 });
