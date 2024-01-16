9d93b34510d2db6a21e466364085921ca81ead8d
dianligegege <1220671124@qq.com>
2024-01-16 23:54:43 +0800

    fix: 修改接口域名

--- src/views/SocketText.vue
+++ src/views/SocketText.vue
@@ -25,7 +25,7 @@ const roomId = 'room1';
 // 随机username
 const userName = `user${Math.floor(Math.random() * 100)}`;
 
-const socket = io('https://10.17.25.252:3000');
+const socket = io('https://web-rtc-server-demo-pp39sv8s7-dianligegeges-projects.vercel.app/');
 const messages = ref<Message[]>([]);
 const newMessage = ref('');
 
